05b0f50b3e98a82b78da52881e1516fdff00a044
miaomm <50197630+aa200647963@users.noreply.github.com>
2022-11-13 00:14:05 +0800

    Create PENET.py

---
+++ PENET.py
@@ -0,0 +1,276 @@
+@registry.ROI_RELATION_PREDICTOR.register("PrototypeEmbeddingNetwork")
+class PrototypeEmbeddingNetwork(nn.Module):
+    def __init__(self, config, in_channels):
+        super(PrototypeEmbeddingNetwork, self).__init__()
+
+        self.num_obj_cls = config.MODEL.ROI_BOX_HEAD.NUM_CLASSES
+        self.num_att_cls = config.MODEL.ROI_ATTRIBUTE_HEAD.NUM_ATTRIBUTES
+        self.num_rel_cls = config.MODEL.ROI_RELATION_HEAD.NUM_CLASSES
+        self.cfg = config
+
+        assert in_channels is not None
+        self.in_channels = in_channels
+        self.obj_dim = in_channels
+        
+
+        self.use_vision = config.MODEL.ROI_RELATION_HEAD.PREDICT_USE_VISION
+        statistics = get_dataset_statistics(config)
+
+        obj_classes, rel_classes, att_classes = statistics['obj_classes'], statistics['rel_classes'], statistics[
+            'att_classes']
+        assert self.num_obj_cls == len(obj_classes)
+        assert self.num_att_cls == len(att_classes)
+        assert self.num_rel_cls == len(rel_classes)
+        self.obj_classes = obj_classes
+        self.rel_classes = rel_classes
+        self.num_obj_classes = len(obj_classes)
+        
+        self.hidden_dim = config.MODEL.ROI_RELATION_HEAD.CONTEXT_HIDDEN_DIM 
+        self.pooling_dim = config.MODEL.ROI_RELATION_HEAD.CONTEXT_POOLING_DIM
+
+        self.mlp_dim = config.MODEL.ROI_RELATION_HEAD.PENET_MLP_DIM
+        self.post_emb = nn.Linear(self.obj_dim, self.mlp_dim * 2)  
+
+        self.embed_dim = config.MODEL.ROI_RELATION_HEAD.PENET_EMBED_DIM
+        dropout_p = config.MODEL.ROI_RELATION_HEAD.PENET_DROPOUT
+        
+        
+        obj_embed_vecs = obj_edge_vectors(obj_classes, wv_dir=self.cfg.GLOVE_DIR, wv_dim=self.embed_dim)  # load Glove for objects
+        rel_embed_vecs = rel_vectors(rel_classes, wv_dir=config.GLOVE_DIR, wv_dim=self.embed_dim)   # load Glove for predicates
+        self.obj_embed = nn.Embedding(self.num_obj_cls, self.embed_dim)
+        self.rel_embed = nn.Embedding(self.num_rel_cls, self.embed_dim)
+        with torch.no_grad():
+            self.obj_embed.weight.copy_(obj_embed_vecs, non_blocking=True)
+            self.rel_embed.weight.copy_(rel_embed_vecs, non_blocking=True)
+       
+        self.W_sub = MLP(self.embed_dim, self.mlp_dim // 2, self.mlp_dim, 2)
+        self.W_obj = MLP(self.embed_dim, self.mlp_dim // 2, self.mlp_dim, 2)
+        self.W_pred = MLP(self.embed_dim, self.mlp_dim // 2, self.mlp_dim, 2)
+        self.project_head = MLP(self.mlp_dim, self.mlp_dim, self.mlp_dim*2, 2)
+
+        self.linear_sub = nn.Linear(self.mlp_dim, self.mlp_dim)
+        self.linear_obj = nn.Linear(self.mlp_dim, self.mlp_dim)
+        self.linear_pred = nn.Linear(self.mlp_dim, self.mlp_dim)
+        self.linear_rel_rep = nn.Linear(self.mlp_dim, self.mlp_dim)
+        
+        self.norm_sub = nn.LayerNorm(self.mlp_dim)
+        self.norm_obj = nn.LayerNorm(self.mlp_dim)
+        self.dropout_sub = nn.Dropout(dropout_p)
+        self.dropout_obj = nn.Dropout(dropout_p)
+        self.norm_rel_labels = nn.LayerNorm(self.mlp_dim)
+        self.norm_union = nn.LayerNorm(self.mlp_dim)
+        self.norm_rel_rep = nn.LayerNorm(self.mlp_dim)
+        self.dropout_rel_rep = nn.Dropout(dropout_p)
+
+        self.dropout_rel = nn.Dropout(dropout_p)
+        self.dropout_pred = nn.Dropout(dropout_p)
+       
+        self.down_samp = MLP(self.pooling_dim, self.mlp_dim, self.mlp_dim, 2) 
+
+        self.gate_sub = nn.Linear(self.mlp_dim*2, self.mlp_dim)  
+        self.gate_obj = nn.Linear(self.mlp_dim*2, self.mlp_dim)
+        self.gate_pred = nn.Linear(self.mlp_dim*2, self.mlp_dim)
+
+        self.vis2sem = nn.Sequential(*[
+            nn.Linear(self.mlp_dim, self.mlp_dim*2), nn.ReLU(True),
+            nn.Dropout(dropout_p), nn.Linear(self.mlp_dim*2, self.mlp_dim)
+        ])
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+
+        ##### refine object labels
+        self.pos_embed = nn.Sequential(*[
+            nn.Linear(9, 32), nn.BatchNorm1d(32, momentum= 0.001),
+            nn.Linear(32, 128), nn.ReLU(inplace=True),
+        ])
+
+        self.obj_embed1 = nn.Embedding(self.num_obj_classes, self.embed_dim)
+        with torch.no_grad():
+            self.obj_embed1.weight.copy_(obj_embed_vecs, non_blocking=True)
+
+        self.obj_dim = in_channels
+        self.out_obj = make_fc(self.hidden_dim, self.num_obj_classes) 
+        self.lin_obj_cyx = make_fc(self.obj_dim + self.embed_dim + 128, self.hidden_dim)
+
+        if self.cfg.MODEL.ROI_RELATION_HEAD.USE_GT_BOX:
+            if self.cfg.MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL:
+                self.mode = 'predcls'
+            else:
+                self.mode = 'sgcls'
+        else:
+            self.mode = 'sgdet'
+
+
+    def forward(self, feature_map, proposals, rel_pair_idxs, rel_labels, rel_binarys, roi_features, union_features, logger=None):
+
+        add_losses = {}
+        add_data = {}
+
+        # refine object labels
+        entity_dists, entity_preds = self.refine_obj_labels(roi_features, proposals)
+        ##### 
+
+        entity_rep = self.post_emb(roi_features)   # using the roi features obtained from the faster rcnn
+        entity_rep = entity_rep.view(entity_rep.size(0), 2, self.mlp_dim)
+
+        sub_rep = entity_rep[:, 1].contiguous().view(-1, self.mlp_dim)    # xs
+        obj_rep = entity_rep[:, 0].contiguous().view(-1, self.mlp_dim)    # xo
+
+        entity_embeds = self.obj_embed(entity_preds) # obtaining the word embedding of entities with GloVe 
+
+        num_rels = [r.shape[0] for r in rel_pair_idxs]
+        num_objs = [len(b) for b in proposals]
+        assert len(num_rels) == len(num_objs)
+
+        sub_reps = sub_rep.split(num_objs, dim=0)
+        obj_reps = obj_rep.split(num_objs, dim=0)
+        entity_preds = entity_preds.split(num_objs, dim=0)
+        entity_embeds = entity_embeds.split(num_objs, dim=0)
+
+        fusion_so = []
+        pair_preds = []
+
+        for pair_idx, sub_rep, obj_rep, entity_pred, entity_embed, proposal in zip(rel_pair_idxs, sub_reps, obj_reps, entity_preds, entity_embeds, proposals):
+
+            s_embed = self.W_sub(entity_embed[pair_idx[:, 0]])  #  Ws x ts
+            o_embed = self.W_obj(entity_embed[pair_idx[:, 1]])  #  Wo x to
+
+            sem_sub = self.vis2sem(sub_rep[pair_idx[:, 0]])  # h(xs)
+            sem_obj = self.vis2sem(obj_rep[pair_idx[:, 1]])  # h(xo)
+            
+            gate_sem_sub = torch.sigmoid(self.gate_sub(cat((s_embed, sem_sub), dim=-1)))  # gs
+            gate_sem_obj = torch.sigmoid(self.gate_obj(cat((o_embed, sem_obj), dim=-1)))  # go
+
+            sub = s_embed + sem_sub * gate_sem_sub  # s = Ws x ts + gs · h(xs)  i.e., s = Ws x ts + vs
+            obj = o_embed + sem_obj * gate_sem_obj  # o = Wo x to + go · h(xo)  i.e., o = Wo x to + vo
+
+            ##### for the model convergence
+            sub = self.norm_sub(self.dropout_sub(torch.relu(self.linear_sub(sub))) + sub)
+            obj = self.norm_obj(self.dropout_obj(torch.relu(self.linear_obj(obj))) + obj)
+            #####
+
+            fusion_so.append(fusion_func(sub, obj)) # F(s, o)
+            pair_preds.append(torch.stack((entity_pred[pair_idx[:, 0]], entity_pred[pair_idx[:, 1]]), dim=1))
+
+        fusion_so = cat(fusion_so, dim=0)  
+        pair_pred = cat(pair_preds, dim=0) 
+
+        sem_pred = self.vis2sem(self.down_samp(union_features))  # h(xu)
+        gate_sem_pred = torch.sigmoid(self.gate_pred(cat((fusion_so, sem_pred), dim=-1)))  # gp
+
+        rel_rep = fusion_so - sem_pred * gate_sem_pred  #  F(s,o) - gp · h(xu)   i.e., r = F(s,o) - up
+        predicate_proto = self.W_pred(self.rel_embed.weight)  # c = Wp x tp  i.e., semantic prototypes
+        
+        ##### for the model convergence
+        rel_rep = self.norm_rel_rep(self.dropout_rel_rep(torch.relu(self.linear_rel_rep(rel_rep))) + rel_rep)
+
+        rel_rep = self.project_head(self.dropout_rel(torch.relu(rel_rep)))
+        predicate_proto = self.project_head(self.dropout_pred(torch.relu(predicate_proto)))
+        ######
+
+        rel_rep_norm = rel_rep / rel_rep.norm(dim=1, keepdim=True)  # r_norm
+        predicate_proto_norm = predicate_proto / predicate_proto.norm(dim=1, keepdim=True)  # c_norm
+
+        ### (Prototype-based Learning  ---- cosine similarity) & (Relation Prediction)
+        rel_dists = rel_rep_norm @ predicate_proto_norm.t() * self.logit_scale.exp()  #  <r_norm, c_norm> / τ
+        # the rel_dists will be used to calculate the Le_sim with the ce_loss
+
+        entity_dists = entity_dists.split(num_objs, dim=0)
+        rel_dists = rel_dists.split(num_rels, dim=0)
+        #################
+
+        if self.training:
+
+            ### Prototype Regularization  ---- cosine similarity
+            target_rpredicate_proto_norm = predicate_proto_norm.clone().detach() 
+            simil_mat = predicate_proto_norm @ target_rpredicate_proto_norm.t()  # Semantic Matrix S = C_norm @ C_norm.T
+            l21 = torch.norm(torch.norm(simil_mat, p=2, dim=1), p=1) / (51*51)  
+            add_losses.update({"l21_loss": l21})  # Le_sim = ||S||_{2,1}
+            ### end
+            
+            ### Prototype Regularization  ---- Euclidean distance
+            gamma2 = 7.0
+            predicate_proto_a = predicate_proto.unsqueeze(dim=1).expand(-1, 51, -1) 
+            predicate_proto_b = predicate_proto.detach().unsqueeze(dim=0).expand(51, -1, -1)
+            proto_dis_mat = (predicate_proto_a - predicate_proto_b).norm(dim=2) ** 2  # Distance Matrix D, dij = ||ci - cj||_2^2
+            sorted_proto_dis_mat, _ = torch.sort(proto_dis_mat, dim=1)
+            topK_proto_dis = sorted_proto_dis_mat[:, :2].sum(dim=1) / 1   # obtain d-, where k2 = 1
+            dist_loss = torch.max(torch.zeros(51).cuda(), -topK_proto_dis + gamma2).mean()  # Lr_euc = max(0, -(d-) + gamma2)
+            add_losses.update({"dist_loss2": dist_loss})
+            ### end 
+
+            ###  Prototype-based Learning  ---- Euclidean distance
+            rel_labels = cat(rel_labels, dim=0)
+            gamma1 = 1.0
+            rel_rep_expand = rel_rep.unsqueeze(dim=1).expand(-1, 51, -1)  # r
+            predicate_proto_expand = predicate_proto.unsqueeze(dim=0).expand(rel_labels.size(0), -1, -1)  # ci
+            distance_set = (rel_rep_expand - predicate_proto_expand).norm(dim=2) ** 2    # Distance Set G, gi = ||r-ci||_2^2
+            mask_neg = torch.ones(rel_labels.size(0), 51).cuda()  
+            mask_neg[torch.arange(rel_labels.size(0)), rel_labels] = 0
+            distance_set_neg = distance_set * mask_neg
+            distance_set_pos = distance_set[torch.arange(rel_labels.size(0)), rel_labels]  # gt i.e., g+
+            sorted_distance_set_neg, _ = torch.sort(distance_set_neg, dim=1)
+            topK_sorted_distance_set_neg = sorted_distance_set_neg[:, :11].sum(dim=1) / 10  # obtaining g-, where k1 = 10, 
+            loss_sum = torch.max(torch.zeros(rel_labels.size(0)).cuda(), distance_set_pos - topK_sorted_distance_set_neg + gamma1).mean()
+            add_losses.update({"loss_dis": loss_sum})     # Le_euc = max(0, (g+) - (g-) + gamma1)
+            ### end 
+ 
+        return entity_dists, rel_dists, add_losses, add_data
+
+
+    def refine_obj_labels(self, roi_features, proposals):
+
+        use_gt_label = self.training or self.cfg.MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL
+        obj_labels = cat([proposal.get_field("labels") for proposal in proposals], dim=0) if use_gt_label else None
+        pos_embed = self.pos_embed(encode_box_info(proposals))
+
+        # label/logits embedding will be used as input
+        if self.cfg.MODEL.ROI_RELATION_HEAD.USE_GT_OBJECT_LABEL:
+            obj_labels = obj_labels.long()
+            obj_embed = self.obj_embed1(obj_labels)
+        else:
+            obj_logits = cat([proposal.get_field("predict_logits") for proposal in proposals], dim=0).detach()
+            obj_embed = F.softmax(obj_logits, dim=1) @ self.obj_embed1.weight
+
+        assert proposals[0].mode == 'xyxy'
+
+        pos_embed = self.pos_embed(encode_box_info(proposals))
+        num_objs = [len(p) for p in proposals]
+        obj_pre_rep_for_pred = self.lin_obj_cyx(cat([roi_features, obj_embed, pos_embed], -1))
+
+
+        if self.mode == 'predcls':
+            obj_labels = obj_labels.long()
+            obj_preds = obj_labels
+            obj_dists = to_onehot(obj_preds, self.num_obj_classes)
+        else:
+            obj_dists = self.out_obj(obj_pre_rep_for_pred)  # 512 -> 151
+            use_decoder_nms = self.mode == 'sgdet' and not self.training
+            if use_decoder_nms:
+                boxes_per_cls = [proposal.get_field('boxes_per_cls') for proposal in proposals]
+                obj_preds = self.nms_per_cls(obj_dists, boxes_per_cls, num_objs).long()
+            else:
+                obj_preds = (obj_dists[:, 1:].max(1)[1] + 1).long()
+        
+        return obj_dists, obj_preds
+
+    def nms_per_cls(self, obj_dists, boxes_per_cls, num_objs):
+        obj_dists = obj_dists.split(num_objs, dim=0)
+        obj_preds = []
+        for i in range(len(num_objs)):
+            is_overlap = nms_overlaps(boxes_per_cls[i]).cpu().numpy() >= self.nms_thresh # (#box, #box, #class)
+
+            out_dists_sampled = F.softmax(obj_dists[i], -1).cpu().numpy()
+            out_dists_sampled[:, 0] = -1
+
+            out_label = obj_dists[i].new(num_objs[i]).fill_(0)
+
+            for i in range(num_objs[i]):
+                box_ind, cls_ind = np.unravel_index(out_dists_sampled.argmax(), out_dists_sampled.shape)
+                out_label[int(box_ind)] = int(cls_ind)
+                out_dists_sampled[is_overlap[box_ind,:,cls_ind], cls_ind] = 0.0
+                out_dists_sampled[box_ind] = -1.0 # This way we won't re-sample
+
+            obj_preds.append(out_label.long())
+        obj_preds = torch.cat(obj_preds, dim=0)
+        return obj_preds
+
